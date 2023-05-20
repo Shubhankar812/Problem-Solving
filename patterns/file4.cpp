87ff599d7803630c779c300c7e940abec2fccc01
Shubhankar812 <shubhankarpande200@gmail.com>
2023-05-20 22:19:00 +0530

    added some more pattern programs on 20th May

---
+++ patterns/file4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int row,col;
+   cin>>row>>col;
+
+   for(int i=row;i>0;i--) {
+      for(int j=0; j<i ;j++) {
+        cout<<"* ";
+      }
+      cout<<endl;
+   }
+}
---
+++ patterns/file5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int row,col;
+    cin>>row>>col;
+
+    for(int i=row; i>=1 ; i--){
+        for(int j=1;j<=i;j++) {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
