ef60aa0cbf3d57a3ed92ea2236930f2e6daae12a
Shubhankar812 <shubhankarpande200@gmail.com>
2023-05-23 14:09:40 +0530

    New pattern file added on 23rd May

---
+++ patterns/file18.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+void print_up(int n)
+{
+    int spaces = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < spaces; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        spaces += 2;
+        cout << endl;
+    }
+}
+
+void print_down(int n)
+{
+    int spaces = 2 * n - 2;
+
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < spaces; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        spaces -= 2;
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    print_up(n);
+    print_down(n);
+}
