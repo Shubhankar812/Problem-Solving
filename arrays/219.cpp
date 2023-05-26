63fa4a9ad04d7faea7ce8f7ddb2eebb6eaaaa5ff
Shubhankar812 <shubhankarpande200@gmail.com>
2023-05-26 22:00:11 +0530

    Adding Leetcode 219

---
+++ arrays/219.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    unordered_map<int, int> mpp;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (mpp.count(nums[i]))
+        {
+            if (abs(i - mpp[nums[i]]) <= k)
+            {
+                return true;
+            }
+        }
+        mpp[nums[i]] = i;
+    }
+    return false;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3, 1};
+    int k = 3;
+
+    bool ans = containsNearbyDuplicate(nums,k);
+    if(ans){
+        cout<<"true"<<endl;
+    }
+    else{
+        cout<<"false"<<endl;
+    }
+}
